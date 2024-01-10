52f418899e8d5e8fe6f38b1c8037274286f73ff0
PrabhavSohgaura <prabhavsohgaura333@gmail.com>
2024-01-10 14:43:39 +0530

    added the questions for string

--- string.js
+++ string.js
@@ -9,9 +9,9 @@ function middleSeat(seat) {
   let s = seat.slice(-1);
 
   if (s === "b" || s === "e") {
-    console.log("This is a middle seat");
+    return console.log("This is a middle seat");
   } else {
-    console.log("this is a corner seat or window seat");
+    return console.log("this is a corner seat or window seat");
   }
 }
 
@@ -24,3 +24,19 @@ const passenger = "pRaBhav";
 const passengerLower = passenger.toLowerCase();
 const passengerName = passengerLower[0].toUpperCase() + passengerLower.slice(1);
 console.log(passengerName);
+
+//Q.3 - comparing the email from the user input
+const email = "hello@gmail.com";
+const loginEmail = "  Hello@Gmail.com \n";
+const correctInputForBackend = loginEmail.toLowerCase().trim();
+console.log(correctInputForBackend);
+
+//Q.4 replacing the words in a string
+const priceUS = "288.75$";
+const priceInd = priceUS.replace("$", "Rs");
+console.log(priceInd);
+
+//to replace both the door words we will use regular expressions
+const announcement =
+  "All passengers please come to door 23.Boarding started at door 23!";
+console.log(announcement.replace(/door/g, "gate"));
